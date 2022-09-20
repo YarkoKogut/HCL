a8d606f08dc36dc46a0b763262b1d81aa45a8275
yarko.kogut <yarko.kogut@gmail.com>
2022-09-19 20:11:00 -0600

    HCL Test module initial commit

---
+++ HCL/Test/Ui/Component/Listing/Column/Test.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright © HCL
+ */
+declare(strict_types=1);
+
+namespace HCL\Test\Ui\Component\Listing\Column;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class Test extends Column
+{
+    private const CUSTOMER_EMAIL = 'customer_email';
+    private const ENTITY_ID = 'entity_id';
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource): array
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $item[$this->getData('name')] = $this->prepareItem($item);
+            }
+        }
+
+        return $dataSource;
+    }
+
+    /**
+     * @param array $item
+     * @return string
+     */
+    private function prepareItem(array $item): string
+    {
+        return $item[self::CUSTOMER_EMAIL] . ' id=' . $item[self::ENTITY_ID];
+    }
+}
---
+++ HCL/Test/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @copyright HCL
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'HCL_Test',
+    __DIR__
+);
